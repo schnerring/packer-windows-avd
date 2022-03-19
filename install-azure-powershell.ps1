dbacecd6083961048949baa7a3a82027e2252ca3
Michael Schnerring <3743342+schnerring@users.noreply.github.com>
2022-03-19 22:44:30 +0100

    Upgrade PowerShell modules

--- install-azure-powershell.ps1
+++ install-azure-powershell.ps1
@@ -1,8 +1,8 @@
-# See https://docs.microsoft.com/en-us/powershell/azure/install-az-ps-msi?view=azps-7.2.0#install-or-update-on-windows-using-the-msi-package
+# See https://docs.microsoft.com/en-us/powershell/azure/install-az-ps-msi?view=azps-7.3.2#install-or-update-on-windows-using-the-msi-package
 
 $ErrorActionPreference = "Stop"
 
-$downloadUrl = "https://github.com/Azure/azure-powershell/releases/download/v7.2.0-February2022/Az-Cmdlets-7.2.0.35201-x64.msi"
+$downloadUrl = "https://github.com/Azure/azure-powershell/releases/download/v7.3.2-March2022/Az-Cmdlets-7.3.2.35305-x64.msi"
 $outFile = "D:\az_pwsh.msi" # ephemeral disk
 
 Write-Host "Installing Azure PowerShell ..."
