65cef78f77e441e64591b52c7d57d6362adf6fde
Michael Schnerring <3743342+schnerring@users.noreply.github.com>
2022-03-27 05:59:42 +0200

    Remove provider.tf file

--- packer.tf
+++ packer.tf
@@ -1,3 +1,28 @@
+terraform {
+  required_version = ">= 1.1.6"
+
+  required_providers {
+    azuread = {
+      source  = "azuread"
+      version = ">= 2.18.0"
+    }
+
+    azurerm = {
+      source  = "azurerm"
+      version = ">= 2.98.0"
+    }
+
+    github = {
+      source  = "integrations/github"
+      version = ">= 4.20.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
 variable "location" {
   type        = string
   description = "Azure region for Packer resources."
